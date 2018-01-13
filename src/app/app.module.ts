cffe396110bae94d8dfa689020c434b371f1afee
Dries Kesteloot <u0109585@kuleuven.be>
2018-01-13 20:14:32 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FooterComponent } from './footer/footer.component';
+import { LightboxComponent } from './lightbox/lightbox.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { MapComponent } from './map/map.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CarouselComponent,
+    FooterComponent,
+    LightboxComponent,
+    PortfolioComponent,
+    PresentationComponent,
+    MapComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
